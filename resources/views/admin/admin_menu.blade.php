72e909633bba799707a646b7bd27a65b833fcbd3
Tyson0723 <t.nagasaki.sys22@morijyobi.ac.jp>
2024-11-22 10:35:16 +0900

    管理者メニューのcssの変更

--- resources/views/admin/admin_menu.blade.php
+++ resources/views/admin/admin_menu.blade.php
@@ -2,19 +2,21 @@
 @section('title', '管理者メニュー')
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin_menu.css') }}">
 @endsection
 @section('content')
 <form action="/" method="post" id="menu_form">
     @csrf
     <input type="radio" name="menu" value="question_add" id="question_add" checked>
-    <label for="question_add">問題登録</label>
+    <label for="question_add">問題登録　　　　新しく問題登録します</label>
     <input type="radio" name="menu" value="list" id="list">
-    <label for="list">問題一覧</label>
+    <label for="list">問題一覧　　　　一覧から編集、削除します</label>
     <input type="radio" name="menu" value="ranking" id="ranking">
-    <label for="ranking">ランキング修正</label>
-    <input type="radio" name="menu" value="admin_add" id="menu">
-    <label for="admin_add">管理者登録</label>
-    <a>ログアウト</a>
+    <label for="ranking">ランキング修正　　ランキングの編集をします</label>
+    <input type="radio" name="menu" value="admin_add" id="admin_add">
+    <label for="admin_add">管理者登録　　　　管理者の登録をします</label>
+    <input type="radio" name="menu" value="logout" id="logout">
+    <label for="logout">　　　　　　　　ログアウト　　　　</label>
     <input type="submit" style="display:none;">
 </form>
 <script src="{{ asset('js/admin_menu.js') }}"></script>
