e3532d329664c7bac03c09175c16de52425c658a
kikuchihibiki <113155569+kikuchihibiki@users.noreply.github.com>
2024-12-13 10:43:29 +0900

    マイグレーションファイルの作成

---
+++ database/migrations/2024_12_13_005633_create_question_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question');
+            $table->string('answer');
+            $table->string('explanation');
+            $table->integer('mode');
+            $table->integer('level');
+            $table->boolean('difficulty_flag')->default(0);
+            $table->boolean('delete_flag')->default(0);
+            $table->boolean('public_flag')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question');
+    }
+};
